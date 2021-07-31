e9e7150ea34a2d844a909cc05ddf128cf9068103
rkiel <cygnus@pobox.com>
2021-07-31 14:18:36 -0400

    student: finished markdown

--- A02-Markdown/start/src/App.js
+++ A02-Markdown/start/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
+  // RENDER STATE
+  const [markdown, setMarkdown] = useState("# sup");
+
+  // FUNCTIONS
+  function onChange(e) {
+    return setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={onChange} value={markdown} />
 
-      <div className="preview" />
+      <ReactMarkdown className="preview" children={markdown} />
     </div>
   );
 }
